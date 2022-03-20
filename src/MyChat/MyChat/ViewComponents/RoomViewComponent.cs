3ec8436a9960118af59020b5e54dde333fb53e09
Руслан Русланов <sharp_blade666@mail.ru>
2022-03-20 17:08:33 +0300

    message styles

---
+++ src/MyChat/MyChat/ViewComponents/RoomViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyChat.Data;
+using MyChat.Models.Rooms;
+
+namespace MyChat.ViewComponents;
+
+public class RoomViewComponent : ViewComponent
+{
+    private readonly ApplicationContext _ctx;
+
+    public RoomViewComponent(ApplicationContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    // чаты, к которым юзер уже приссоединился
+    public IViewComponentResult Invoke()
+    {
+        var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        
+        var chats = _ctx.RoomUsers
+            .Include(x => x.Room)
+            .Where(x => x.UserId == userId
+                        && x.Room.Type == RoomType.Room)
+            .Select(x => x.Room)
+            .ToList();
+
+        return View(chats);
+    }
+}
