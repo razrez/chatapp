c73b5177658f2e11428a2d95bad8f72c0dec420d
Руслан Русланов <sharp_blade666@mail.ru>
2022-03-28 01:13:21 +0300

    теперь все робит, я думаю, что ограничение на ajax было, поэтому первее нужно отправить

--- src/MyChat/MyChat/SignalR/Hubs/ChatHub.cs
+++ src/MyChat/MyChat/SignalR/Hubs/ChatHub.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using MyChat.Controllers;
 using MyChat.Data;
 using MyChat.Models;
 
@@ -9,6 +13,13 @@ namespace MyChat.SignalR.Hubs
     //будет использоваться как сервис в контроллере
     public class ChatHub : Hub
     {
+        private readonly ApplicationContext _applicationContext;
+        private readonly UserManager<User> _userManager;
+        public ChatHub(ApplicationContext applicationContext, UserManager<User> userManager)
+        {
+            _applicationContext = applicationContext;
+            _userManager = userManager;
+        }
         /*public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
@@ -24,19 +35,22 @@ namespace MyChat.SignalR.Hubs
             return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
         }
 
-        public async Task SendMessage(string message, string roomName)
+        public async Task SendMessage(string message, string roomName, string roomId)
         {
-            /*//var sender = await _userManager.FindByNameAsync(user.UserName);
-            var currentRoom = await ctx.Rooms.FindAsync(roomId);
-            //return Content($"{currentRoom.Id} {sender.UserName}: {message}");
-
-            var Mes = new Message() { Name = currentUser.Login, Room = currentRoom!, Text = message, User = currentUser };
-            //await ctx.AddAsync(new Message(){Name = sender.Login, Room = currentRoom!, Text = message, User = sender});
-            await ctx.AddAsync(Mes);
-            await ctx.SaveChangesAsync();*/
-        
             await Clients.Group(roomName)
                 .SendAsync("ReceiveMessage", message);
+            
+            var sender = await _userManager.FindByNameAsync(Context.User?.Identity?.Name);
+            var roomIdInt = Int32.Parse(roomId);
+            var currentRoom = await _applicationContext.Rooms.FindAsync(roomIdInt);
+            //return Content($"{currentRoom.Id} {sender.UserName}: {message}");
+            
+            var mesDb = new Message() { Name = sender.UserName, Room = currentRoom!, Text = message, User = sender };
+            //await ctx.AddAsync(new Message(){Name = sender.Login, Room = currentRoom!, Text = message, User = sender});
+            await _applicationContext.AddAsync(mesDb);
+            await _applicationContext.SaveChangesAsync();
+            
         }
+
     }
 }
